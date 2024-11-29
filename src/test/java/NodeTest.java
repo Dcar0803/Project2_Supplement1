14796c66fc59e83d1d7b387ee173085f65578df8
TwistedWonder2021 <124842528+Dcar0803@users.noreply.github.com>
2024-11-28 20:51:41 -0500

    Test: Created test methods to insert data into the node

--- src/test/java/NodeTest.java
+++ src/test/java/NodeTest.java
@@ -12,4 +12,31 @@ class NodeTest {
         assertNull(node.getLeft());
         assertNull(node.getRight());
     }
+	
+	@Test
+    void testInsertLeft() {
+        Node root = new Node("root", 10);
+        root.insert("left", 5);
+        assertNotNull(root.getLeft());
+        assertEquals(5, root.getLeft().getData());
+        assertEquals("left", root.getLeft().getNodeId());
+
+        @Test
+        void testInsertRight() {
+            Node root = new Node("root", 10);
+            root.insert("right", 15);
+            assertNotNull(root.getRight());
+            assertEquals(15, root.getRight().getData());
+            assertEquals("right", root.getRight().getNodeId());
+        }
+
+        @Test
+        void testInsertRecursive() {
+            Node root = new Node("root", 10);
+            root.insert("left", 5);
+            root.insert("left-left", 3);
+            assertNotNull(root.getLeft().getLeft());
+            assertEquals(3, root.getLeft().getLeft().getData());
+            assertEquals("left-left", root.getLeft().getLeft().getNodeId());
+        }
 }//end of class 
